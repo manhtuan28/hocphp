fa2d1b3c866a16cf51416568874d72d0c112ff7b
Manh Tuan <buimanhtuan2k4@gmail.com>
2025-02-25 08:27:48 +0700

    commit

---
+++ On-Thi-Giua-Ky/B1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mảng</title>
+</head>
+<body>
+    <form action="" method="post">
+        <div class="title">
+            Bài Tập
+        </div>
+        <div class="form-input">
+            <div class="box-input">
+                <input type="text" name="" id="">
+            </div>
+        </div>
+    </form>
+</body>
+</html>
